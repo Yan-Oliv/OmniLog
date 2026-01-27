f03baa89b77411f80ef65f7c81216388eae2e4d9
Oliveira_Yan <yanoliveiragm@gmail.com>
2026-01-27 10:41:20 -0300

    Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "895821785"
+    serviceWorkerVersion: "3251626225"
   }
 });
