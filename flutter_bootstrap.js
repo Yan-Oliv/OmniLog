41d81ab7bcedcfc610ce4a3f8af8a0e22e21f30e
Yan de Oliveira <yanoliveiragm@gmail.com>
2026-03-14 13:06:52 -0300

    Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1124385243" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2876156746" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
