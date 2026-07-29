6d578ba68ed3b6805657f6bba103fa6e263bf8b3
Yan_Oliv <yanoliveiragm@gmail.com>
2026-07-29 14:30:33 -0300

    feat: use unified password recovery endpoint

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2196939668" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3261384215" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
